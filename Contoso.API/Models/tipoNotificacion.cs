c6f0c09551a7eb9829f94548a13358b80f852f52
Isaac Belmont <isaacbelmontv@gmail.com>
2017-12-12 21:59:55 -0600

    notif

---
+++ Contoso.API/Models/tipoNotificacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contoso.API.Models
+{
+    public class tipoNotificacion{
+       
+        public int Id_notif { get; set; }
+
+        public int Id_usuario { get; set; }
+
+        public int Id_notificacion { get; set; }
+    }
+}
